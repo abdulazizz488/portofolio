2b6522e32ee30f42e559a4fec6c89c71b558bc94
abdulazizz488 <aloksupreme07@gmail.com>
2025-12-14 09:02:35 +0700

    update portofolio

--- portofolio.js
+++ portofolio.js
@@ -1,3 +1,33 @@
+let modeLigth = document.getElementById("ligth");
+let buttonMode = document.getElementById("mode");
+let icon = document.getElementById("icon-mode");
+
+// mode default
+modeLigth.href = "portofolio.css"; // default stylesheet
+
+buttonMode.addEventListener("click", function () {
+  let z = modeLigth.classList.contains("apa");
+
+  if (z) {
+    // Light MODE
+    modeLigth.classList.remove("apa");
+    modeLigth.href = "ligth.css";
+
+    icon.className = "ph ph-sun";  // icon cerah
+    icon.style.color = "#4A90E2";  // warna cerah
+  } else {
+    // Dark MODE
+    modeLigth.classList.add("apa");
+    modeLigth.href = "portofolio.css";
+
+    icon.className = "ph ph-moon"; // icon gelap
+    icon.style.color = "white";    // warna gelap
+  }
+});
+
+
+
+
 const cardProject1 = document.getElementById("card-project1");
 
 const project1 = [
@@ -7,53 +37,53 @@ const project1 = [
       "Aplikasi Web untuk menghitung zakat emas",
     image: "/img/codingan.jpeg",
     tags: ["HTML", "CSS", "JavaScript", "LocalStorage"],
-    link: "https://github.com/abdulazizz488/zakat-emas.git",
+    link: "http://127.0.0.1:5500/index.html",
   },
   {
     title: "Kalkulator",
     description: "Aplikasi Web untuk menghitung",
     image: "/img/kalkulator.jpeg",
     tags: ["HTML", "CSS", "JavaScript", "LocalStorage"],
-    link: "https://github.com/abdulazizz488/bikin-kalkulator.git",
+    link: "http://127.0.0.1:5500/kalkulator.html",
   },
   {
     title: "Lampu merah",
     description: "Simulasi Lampu merah",
     image: "/img/lampu (1).jpeg",
     tags: ["HTML", "CSS", "JavaScript", "LocalStorage"],
-    link: "https://github.com/abdulazizz488/traffic-lamp.git",
+    link: "http://127.0.0.1:5500/traffic.html",
   },
 ];
-  
+
 
 for (let i = 0; i < project1.length; i++) {
-    let project = project1[i];
+  let project = project1[i];
 
-    let card = document.createElement("div");
-    card.className = "card";
+  let card = document.createElement("div");
+  card.className = "card";
 
-    let image = document.createElement("img");
-    image.src = project.image;
-    image.className = "project-nya"
+  let image = document.createElement("img");
+  image.src = project.image;
+  image.className = "project-nya"
 
-    let title = document.createElement("h3");
-    title.textContent = project.title;
+  let title = document.createElement("h3");
+  title.textContent = project.title;
 
-    let description = document.createElement("p");
-    description.textContent = project.description;
+  let description = document.createElement("p");
+  description.textContent = project.description;
 
-    let link = document.createElement("a");
-    link.href = project.link;
-    link.target = "blank";
-    link.className = "link"
-    link.textContent = "Lihat Detail"
+  let link = document.createElement("a");
+  link.href = project.link;
+  link.target = "blank";
+  link.className = "link"
+  link.textContent = "Lihat Detail"
 
-    card.appendChild(image);
-    card.appendChild(title);
-    card.appendChild(description)
-    card.appendChild(link)
-    
-    cardProject1.appendChild(card)
+  card.appendChild(image);
+  card.appendChild(title);
+  card.appendChild(description)
+  card.appendChild(link)
+
+  cardProject1.appendChild(card)
 }
 
 
@@ -77,42 +107,42 @@ const project2 = [
   },
   {
     title: "Video I'rab",
-    description: "Video i'rab surat al-ikhlas",
+    description: "Video i'rab surat an-naas",
     image: "/img/lampu (1).jpeg",
     tags: ["HTML", "CSS", "JavaScript", "LocalStorage"],
-    link: "",
+    link: "https://drive.google.com/drive/folders/1M9MfTZ1PeHmY-dWutctLyoPg_twEeAlZ",
   },
 ];
-  
+
 
 for (let i = 0; i < project2.length; i++) {
-    let project = project2[i];
+  let project = project2[i];
 
-    let card = document.createElement("div");
-    card.className = "card";
+  let card = document.createElement("div");
+  card.className = "card";
 
-    let image = document.createElement("img");
-    image.src = project.image;
-    image.className = "project-nya"
+  let image = document.createElement("img");
+  image.src = project.image;
+  image.className = "project-nya"
 
-    let title = document.createElement("h3");
-    title.textContent = project.title;
+  let title = document.createElement("h3");
+  title.textContent = project.title;
 
-    let description = document.createElement("p");
-    description.textContent = project.description;
+  let description = document.createElement("p");
+  description.textContent = project.description;
 
-    let link = document.createElement("a");
-    link.href = project.link;
-    link.target = "blank";
-    link.className = "link"
-    link.textContent = "Lihat Detail"
+  let link = document.createElement("a");
+  link.href = project.link;
+  link.target = "blank";
+  link.className = "link"
+  link.textContent = "Lihat Detail"
 
-    card.appendChild(image);
-    card.appendChild(title);
-    card.appendChild(description)
-    card.appendChild(link)
-    
-    cardProject2.appendChild(card)
+  card.appendChild(image);
+  card.appendChild(title);
+  card.appendChild(description)
+  card.appendChild(link)
+
+  cardProject2.appendChild(card)
 }
 
 
@@ -128,48 +158,50 @@ const project3 = [
     link: "https://drive.google.com/drive/folders/1mujSfdz1VlG13KfEJPxkboDzi8b6Gdy-?usp=sharing",
   },
   {
-    title: "Kalkulator",
-    description: "Aplikasi Web untuk menghitung",
+    title: "Speaking English",
+    description: "Tugas ngomong dengan bahasa inggris",
     image: "/img/kalkulator.jpeg",
     tags: ["HTML", "CSS", "JavaScript", "LocalStorage"],
-    link: "https://github.com/abdulazizz488/bikin-kalkulator.git",
+    link: "https://drive.google.com/drive/folders/1frs7FXOxbXBMLJVQNQrwjLYr70LydjP9",
   },
   {
-    title: "Lampu merah",
-    description: "Simulasi Lampu merah",
-    image: "/img/lampu (1).jpeg",
+    title: "Short Movie",
+    description: "Tugas membuat short movie dengan bahasa inggris",
+    image: "/img/movie2.png",
     tags: ["HTML", "CSS", "JavaScript", "LocalStorage"],
-    link: "https://github.com/abdulazizz488/traffic-lamp.git",
+    link: "https://drive.google.com/drive/folders/1bnUXBbl_FqDgiU94YfmAF176ycYxyLwb",
   },
 ];
-  
+
 
 for (let i = 0; i < project3.length; i++) {
-    let project = project3[i];
+  let project = project3[i];
 
-    let card = document.createElement("div");
-    card.className = "card";
+  let card = document.createElement("div");
+  card.className = "card";
 
-    let image = document.createElement("img");
-    image.src = project.image;
-    image.className = "project-nya"
+  let image = document.createElement("img");
+  image.src = project.image;
+  image.className = "project-nya"
 
-    let title = document.createElement("h3");
-    title.textContent = project.title;
+  let title = document.createElement("h3");
+  title.textContent = project.title;
 
-    let description = document.createElement("p");
-    description.textContent = project.description;
+  let description = document.createElement("p");
+  description.textContent = project.description;
 
-    let link = document.createElement("a");
-    link.href = project.link;
-    link.target = "blank";
-    link.className = "link"
-    link.textContent = "Lihat Detail"
+  let link = document.createElement("a");
+  link.href = project.link;
+  link.target = "blank";
+  link.className = "link"
+  link.textContent = "Lihat Detail"
 
-    card.appendChild(image);
-    card.appendChild(title);
-    card.appendChild(description)
-    card.appendChild(link)
-    
-    cardProject3.appendChild(card)
+  card.appendChild(image);
+  card.appendChild(title);
+  card.appendChild(description)
+  card.appendChild(link)
+
+  cardProject3.appendChild(card)
 }
+
+
